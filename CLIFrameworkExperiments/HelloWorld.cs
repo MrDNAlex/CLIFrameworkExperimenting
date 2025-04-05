bf143d6e43e42eec4083e7ec4e8f3d680c568c28
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-04-05 16:43:27 -0400

    Successful CLI run with a Global Flag

---
+++ CLIFrameworkExperiments/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using NanoDNA.CLIFramework.Commands;
+
+namespace CLIFrameworkExperiments
+{
+    internal class HelloWorld : Command
+    {
+        public override string Name => "hello-world";
+
+        public override string Description => "Says Hello World in the Console";
+
+        public override void Execute(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CLIFrameworkExperiments/Verbose.cs
@@ -0,0 +1,18 @@
+﻿using NanoDNA.CLIFramework.Flags;
+
+namespace CLIFrameworkExperiments
+{
+    public class Verbose : Flag
+    {
+        public Verbose(string[] arguments) : base(arguments)
+        {
+            Console.WriteLine("Verbose Flag Activated");
+        }
+
+        public override string Name => "verbose";
+
+        public override string ShorthandName => "v";
+
+        public override string Description => "Adds a more Verbose Debugging to the CLI App";
+    }
+}
